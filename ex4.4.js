ab2519c218fd400a6f3b4ab4fc4dafd2dd68fb1a
sivanxpeer <sivan.x.peer@gmail.com>
2021-11-09 09:24:39 +0200

    small change

--- ex4.4.js
+++ ex4.4.js
@@ -1,11 +1,11 @@
-function boom(n){
-    for(let i=1;i<=n;i++){
-        if(i%7==0 && i.toString().includes(7)){
+function boom(n) {
+    for (let i = 1; i <= n; i++) {
+        if (i % 7 == 0 && i.toString().includes(7))
             console.log("BOOM-BOOM");
-        }
-        else if(i%7==0){
+
+        else if (i % 7 == 0)
             console.log("BOOM");
-        }
+
         else console.log(i);
     }
 }
