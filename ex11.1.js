f4086908757363ba07e9e7b1041a0fd8b7700f29
sivanxpeer <sivan.x.peer@gmail.com>
2021-11-16 10:06:39 +0200

    ex11.1

---
+++ ex11.1.js
@@ -0,0 +1,53 @@
+const newReleases = [
+    {
+        id: 70111470,
+        title: "Die Hard",
+        boxart: "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+        uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+        rating: [4.0],
+        bookmark: [],
+    },
+    {
+        id: 654356453,
+        title: "Bad Boys",
+        boxart: "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+        uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+        rating: [5.0],
+        bookmark: [{ id: 432534, time: 65876586 }],
+    },
+    {
+        id: 65432445,
+        title: "The Chamber",
+        boxart: "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+        uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+        rating: [4.0],
+        bookmark: [],
+    },
+    {
+        id: 675465,
+        title: "Fracture",
+        boxart: "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+        uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+        rating: [5.0],
+        bookmark: [{ id: 432534, time: 65876586 }],
+    },
+];
+
+newReleases.forEach(function(movie){
+    let obj = {};
+    let resArr = [];
+    obj["id"] = movie.id;
+    obj["title"] = movie.title;
+    resArr.push(obj);
+    // console.log(resArr);    
+    return resArr;
+})
+
+const moviesNamesAndIds = newReleases.map(function(movie){
+    let obj ={};
+    obj['id'] = movie.id;
+    obj['title']= movie.title;
+    return obj;
+})
+
+console.log(moviesNamesAndIds);
